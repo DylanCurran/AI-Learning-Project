ceaa23729a21d3afa50c42dad382b14f88203c98
Dylan Curran <c00217112@itcarlow.ie>
2019-12-09 21:34:08 +0000

    First Upload, forgot to set up repo

---
+++ AI Learning Project/AI Learning Project/Assets/FindPlayer.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FindPlayer : MonoBehaviour
+{
+	public GameObject player;
+
+	bool notColliding;
+	// Start is called before the first frame update
+	private void Start()
+	{
+		notColliding = true;
+	}
+
+	// Update is called once per frame
+	void Update()
+    {
+		if (notColliding)
+		{
+			this.transform.position += (player.transform.position - this.transform.position) / 10 * 1 / 60.0f;
+		}
+		else
+		{
+			Debug.Log("colliding");
+		}
+    }
+
+	void OnColliderEnter(Collider other)
+	{
+		Debug.Log("Collision Entered");
+		//if (other.tag == "Wall")
+		//{
+			notColliding = false;
+			this.transform.position += new Vector3(0.0f, 0.0f, 10.0f);
+
+		//}
+	}
+
+	void OnColliderExit(Collider other)
+	{
+		Debug.Log("Collision Exited");
+		notColliding = true;
+	}
+}
